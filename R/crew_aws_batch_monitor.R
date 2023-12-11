7413f8a9e9eb023c6c59779086de09df015e21ac
wlandau-lilly <will.landau@lilly.com>
2023-12-11 16:11:03 -0500

    Fix CI test

--- R/crew_aws_batch_monitor.R
+++ R/crew_aws_batch_monitor.R
@@ -382,8 +382,7 @@ crew_class_aws_batch_monitor <- R6::R6Class(
       fields <- c(
         ".job_queue",
         ".job_definition",
-        ".log_group",
-        ".region"
+        ".log_group"
       )
       for (field in fields) {
         crew::crew_assert(
@@ -395,14 +394,16 @@ crew_class_aws_batch_monitor <- R6::R6Class(
           message = paste("invalid", field)
         )
       }
-      crew::crew_assert(
-        private$.endpoint %|||% "x",
-        is.character(.),
-        !anyNA(.),
-        length(.) == 1L,
-        nzchar(.),
-        message = "endpoint must be NULL or a character of length 1."
-      )
+      for (field in c(".endpoint", ".region")) {
+        crew::crew_assert(
+          private[[field]] %|||% "x",
+          is.character(.),
+          !anyNA(.),
+          length(.) == 1L,
+          nzchar(.),
+          message = paste(field, "must be NULL or a character of length 1.")
+        )
+      }
       for (field in c(".config", ".credentials")) {
         crew_assert(
           private[[field]] %|||% list(),
